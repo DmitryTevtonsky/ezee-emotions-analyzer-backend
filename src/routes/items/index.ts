7dd7f9e0fb32720e32a71f9202c33ed8a6dba839
DmitryTevtonsky <tevtonsky1054m1@gmail.com>
2022-06-25 17:29:24 +0300

    Fix.

--- src/routes/items/index.ts
+++ src/routes/items/index.ts
@@ -8,9 +8,6 @@ const axiosInstanceDS = axios.create({
     baseURL: 'http://127.0.0.1:5001', // DS сервис
 });
 
-const calculateClassData = (data) => {
-
-}
 
 const initRoutes = (app: Application) => {
 
