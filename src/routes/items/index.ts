b81eaecbcfe93e4a09e431439a55fd3a2ea9ba27
DmitryTevtonsky <tevtonsky1054m1@gmail.com>
2022-06-25 17:28:53 +0300

    Fix.

--- src/routes/items/index.ts
+++ src/routes/items/index.ts
@@ -8,6 +8,10 @@ const axiosInstanceDS = axios.create({
     baseURL: 'http://127.0.0.1:5001', // DS сервис
 });
 
+const calculateClassData = (data) => {
+
+}
+
 const initRoutes = (app: Application) => {
 
     // Принимаем входное видео с фронта, сохраняем видео и отправляем его на анализ в DS
@@ -18,7 +22,7 @@ const initRoutes = (app: Application) => {
                 filename: req.file.path
             });
 
-            console.log({ status, data });
+            console.log('response from DS', { status, data });
 
             res.send({
                 path: req.file.path
@@ -30,11 +34,15 @@ const initRoutes = (app: Application) => {
 
     // Принимаем результат формирования "класса" из DS сервиса
     app.post(`${apiPrefix}/result-class`, (req, res) => {
-        console.log(req.body);
+        console.log('/result-class', req.body);
+
+        res.send({});
     });
 
     app.post(`${apiPrefix}/init-emotion-analyze`, (req, res) => {
         console.log(req.body);
+
+        res.send({});
     });
 }
 
